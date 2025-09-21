6dc240a5faa9597e1efe0723c4a40b4df94b92b2
AIAnupSHrestha <shresthanup886@gmail.com>
2025-09-21 11:53:33 +0545

    feat: implement backend for document ingestion, RAG, and interview booking

--- tes.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-from services.RedisMemory import get_conversation
-
-load_dotenv() 
-
-print(os.getenv("REDIS_PORT"))
-print(get_conversation(session_id="user123"))
